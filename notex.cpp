571ea8ee70e692a2d3e7fe81d75a01f981fbb18e
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2021-05-19 15:18:14 +0600

    Add notex print feature Why: to saperately print notex datas How: using the print func What: Tags: notex

--- notex.cpp
+++ notex.cpp
@@ -14,18 +14,32 @@ int main(int argc, char const *argv[]) {
   const char *tbname = "general";
   Database *db = new Database();
 
-  db->createDb(dbfilename);
-  db->createNotexTable(dbfilename, tbname);
-  std::string note;
-  std::cout << "Extra Notes: ";
-  getline(std::cin, note);
-  bool queryStatus = db->insertNotexData(dbfilename, tbname, note.c_str());
-  if (queryStatus)
-    alib::decorateMe("Records kept successfully", 0, "*");
-  else
-    std::cerr << rang::fg::red << "Errors occured ! Couldn't store data"
-              << rang::fg::reset << std::endl;
+  if (argc >= 2) {
+    const char *availableCommands[] = {"-p", "--print"};
 
+    if (std::strcmp(argv[1], availableCommands[0]) == 0 ||
+        std::strcmp(argv[1], availableCommands[1]) ==
+            0) { /* -p || --print option */
+      db->printNotexData(dbfilename, tbname);
+    } else {
+      alib::horizontalLine(1, "red");
+      std::cout << rang::fg::red << "Inavlid Arguments Passed"
+                << rang::fg::reset << std::endl;
+      alib::horizontalLine(1, "red");
+    }
+  } else {
+    db->createDb(dbfilename);
+    db->createNotexTable(dbfilename, tbname);
+    std::string note;
+    std::cout << "Extra Notes: ";
+    getline(std::cin, note);
+    bool queryStatus = db->insertNotexData(dbfilename, tbname, note.c_str());
+    if (queryStatus)
+      alib::decorateMe("Records kept successfully", 0, "*");
+    else
+      std::cerr << rang::fg::red << "Errors occured ! Couldn't store data"
+                << rang::fg::reset << std::endl;
+  }
   delete db;
   return 0;
 }
